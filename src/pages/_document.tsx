6ee1994cc339318c2358636787306f91f487b36c
faridhash26 <faridhash26@gmail.com>
2023-01-21 09:34:48 -0800

    adding some metatag  in the link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,24 @@ class MyDocument extends Document {
         <Head>
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#fff" />
+          <meta name="application-name" content="PWA App" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="PWA App" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta
+            name="msapplication-config"
+            content="/icons/browserconfig.xml"
+          />
+          <meta name="msapplication-TileColor" content="#2B5797" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="PWA App" />
+          <meta property="og:description" content="Best PWA App in the world" />
+          <meta property="og:site_name" content="PWA App" />
         </Head>
         <body>
           <Main />
