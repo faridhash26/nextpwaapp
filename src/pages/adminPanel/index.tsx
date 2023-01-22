7d7bb0103af036e730edc188a41fd01f8bb47833
faridhash26 <faridhash26@gmail.com>
2023-01-22 09:18:12 -0800

    init the tailwin style

--- src/pages/adminPanel/index.tsx
+++ src/pages/adminPanel/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import type { ReactElement } from "react";
 
 import AdminLayout from "components/layout/adminpanel/AdminLayout";
-import type { NextPageWithLayout } from '../_app'
+import type { NextPageWithLayout } from "../_app";
 
 const AdminPage = () => {
-  return <div>index</div>;
+  return <div className="bg-red text-3xl font-bold underline">index</div>;
 };
 
 export default AdminPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <>Home</>;
+  return <div className="border bg-red p-5">Home</div>;
 };
 
 export default Home;
