eca7ec3f32f4d89fcb729c1781f0b8bd9af90226
faridhash26 <faridhash26@gmail.com>
2023-01-24 10:23:24 -0800

    adding the custom switch

--- components/layout/adminpanel/Header.tsx
+++ components/layout/adminpanel/Header.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import { FiMoreHorizontal } from "react-icons/fi";
 import { AiOutlineMenu } from "react-icons/ai";
+import CustomSwitch from "components/pureElement/CustomSwitch";
 
 const Header = () => {
+  const [first, setfirst] = useState(false);
   return (
     <header className="flex items-center justify-between p-4 dark:bg-darker">
       <div className="flex items-center space-x-4 md:space-x-0">
         <button className="p-1 text-indigo-400 transition-colors duration-200 rounded-md bg-indigo-50  hover:text-indigo-600 hover:bg-indigo-100 dark:hover:text-light dark:hover:bg-indigo-700 dark:bg-maindark focus:outline-none focus:ring">
           <span className="sr-only">Open main manu</span>
-
           {/* <span aria-hidden="true"> */}
           <span>
             <FiMoreHorizontal size={25} />
@@ -24,6 +25,7 @@ const Header = () => {
             <BsSearch size={25} />
           </span>
         </button>
+        <CustomSwitch isChecked={first} setIsChecked={setfirst} />
 
         <button className="p-1 text-indigo-400 transition-colors duration-200 rounded-md bg-indigo-50 xl:hidden hover:text-indigo-600 hover:bg-indigo-100 dark:hover:text-light dark:hover:bg-indigo-700 dark:bg-maindark focus:outline-none focus:ring">
           <span className="sr-only">Open user panel</span>
--- components/pureElement/CustomSwitch.tsx
+++ components/pureElement/CustomSwitch.tsx
@@ -1,42 +1,29 @@
-import React from "react";
+import React, { ChangeEvent, FC } from "react";
 
-const CustomSwitch = () => {
+interface CustomSwitchProps {
+  isChecked: boolean;
+  isdisabled?: boolean;
+  setIsChecked: (isChecked: boolean) => void;
+}
+const CustomSwitch: FC<CustomSwitchProps> = ({
+  isChecked,
+  isdisabled,
+  setIsChecked,
+}) => {
+  const onchangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(e.target.checked);
+  };
   return (
-    <button aria-hidden="true" className="relative focus:outline-none" x-cloak>
-      <div className="w-12 h-6 transition bg-indigo-100 rounded-full outline-none dark:bg-indigo-400"></div>
-      <div className="absolute top-0 left-0 inline-flex items-center justify-center w-6 h-6 transition-all duration-150 transform scale-110 rounded-full shadow-sm">
-        <svg
-          x-show="!isDark"
-          className="w-4 h-4"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-          />
-        </svg>
-        <svg
-          x-show="isDark"
-          className="w-4 h-4"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-          />
-        </svg>
-      </div>
-    </button>
+    <label className="relative inline-flex items-center cursor-pointer">
+      <input
+        type="checkbox"
+        onChange={onchangeHandler}
+        checked={isChecked}
+        className="sr-only peer"
+        disabled={isdisabled}
+      />
+      <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+    </label>
   );
 };
 
