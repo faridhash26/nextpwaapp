5f696429f9b0b2a4e1ca10f18a3c479a0d9f231a
faridhash26 <faridhash26@gmail.com>
2023-01-26 07:26:38 -0800

    delete error in the index admin panel

--- src/pages/adminPanel/index.tsx
+++ src/pages/adminPanel/index.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import type { ReactElement } from "react";
 
 import AdminLayout from "components/layout/adminpanel/AdminLayout";
-import type { NextPageWithLayout } from "../_app";
-import { useSelector } from "react-redux";
+
 
 const AdminPage = () => {
   return <div className="bg-red text-3xl font-bold underline">index</div>;
@@ -11,5 +10,5 @@ const AdminPage = () => {
 
 export default AdminPage;
 AdminPage.getLayout = function getLayout(page: ReactElement) {
-  return <AdminLayout userSettings>{page}</AdminLayout>;
+  return <AdminLayout>{page}</AdminLayout>;
 };
